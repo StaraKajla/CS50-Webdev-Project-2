6a82b5803d1888028a8a1b64ce69643ff55a793c
Domagoj Kalšan <domagojkalsan@gmail.com>
2022-10-24 21:50:44 +0200

    v1

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/", views.index, name="wiki"),
+    path("wiki/<str:name>", views.title, name="title"),
+    path("query/", views.query, name="query"),
+    path("new/", views.newPage, name="new"),
+    path("save/", views.save, name="save"),
+    path("randomSite/", views.randomSite, name="randomPage"),
+    path("edit/", views.edit, name="edit")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import random
+
 
 from . import util
 
@@ -8,3 +10,74 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki(request):
+    return render(request, "encyclopedia/wiki.html", {
+        "entries": util.list_entries()
+    })
+
+def title(request, name):
+    if not util.get_entry(name):
+        return render(request, "encyclopedia/notfound.html", {
+            "title": name.capitalize(),
+            "missingPage": name
+        })
+        
+    return render(request, f"encyclopedia/titleSearch.html", {
+        "entry": util.get_entry(name),
+        "title": name.capitalize()
+    })
+
+def query(request):
+    q = request.GET.get('q')
+    match = util.get_entry(q)
+    if match:
+        return render(request, "encyclopedia/titleSearch.html", {
+            "entry": match,
+            "title": q.capitalize()
+        })
+
+    else:
+        similarEntries = []
+        for entry in util.list_entries():
+            if q.lower() in entry.lower():
+                similarEntries.append(entry)
+
+        return render(request, "encyclopedia/searchresults.html", {
+            "entries": similarEntries,
+            "searchParam": q
+    })
+    
+def newPage(request):
+    return render(request, "encyclopedia/newpage.html")
+
+def save(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+
+    match = util.get_entry(title)
+    if match:
+        return render(request, "encyclopedia/page_exists.html", {
+            "title": title.capitalize()
+        })
+
+    util.save_entry(title, content)
+
+    return render(request, "encyclopedia/titleSearch.html", {
+        "entry": util.get_entry(title),
+        "title": title.capitalize()
+    })
+
+# TODO
+def edit(request):
+    return render(request, "encyclopedia/index.html")
+
+def randomSite(request):
+    entries = util.list_entries()
+    allEntries = len(entries) - 1
+    x = random.randint(0, allEntries)
+
+    return render(request, "encyclopedia/titleSearch.html", {
+        "entry": util.get_entry(entries[x]),
+        "title": entries[x]
+    })
